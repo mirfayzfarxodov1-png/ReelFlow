cec10b593062a49e5edd25ebe6cbacf20a13f378
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-06-03 23:33:39 +0500

    Create Message.js

---
+++ backend/models/Message.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    video: {
+        type: String,
+        default: ''
+    },
+    voice: {
+        type: String,
+        default: ''
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
