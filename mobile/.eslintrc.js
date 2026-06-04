69fb64621369348e333c730213772d6412a500f3
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-06-04 21:07:51 +0500

    Add ESLint configuration for React Native project

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'no-trailing-spaces': 'off',
+    'comma-dangle': 'off',
+    'eol-last': 'off',
+    'prettier/prettier': 'off',
+  },
+};
