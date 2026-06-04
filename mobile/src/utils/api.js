43eec688c642429e07297e9763b954a0ea9d2000
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-06-04 20:46:42 +0500

    Refactor API utility for AsyncStorage and URL update
    Updated API utility to use AsyncStorage for token management and modified API URL for Android emulator.

--- mobile/src/utils/api.js
+++ mobile/src/utils/api.js
@@ -1,67 +1,37 @@
 import axios from 'axios';
-import { getToken } from './storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api'; // Android emulator
+const API_URL = 'http://10.0.2.2:5000/api'; // Android emulator
+// const API_URL = 'http://127.0.0.1:5000/api'; // iOS simulator
 
 const api = axios.create({
   baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' }
 });
 
-api.interceptors.request.use(async (config) => {
-  const token = await getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await AsyncStorage.multiRemove(['token', 'user']);
+      // Navigate to login
+    }
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
-export const login = async (email, password) => {
-  const response = await api.post('/auth/login', { email, password });
-  return response.data;
-};
-
-export const register = async (username, email, password) => {
-  const response = await api.post('/auth/register', { username, email, password });
-  return response.data;
-};
-
-export const getFeed = async (type) => {
-  const response = await api.get('/videos/feed');
-  return response.data;
-};
-
-export const getVideo = async (id) => {
-  const response = await api.get(`/videos/${id}`);
-  return response.data;
-};
-
-export const likeVideo = async (id) => {
-  const response = await api.put(`/videos/${id}/like`);
-  return response.data;
-};
-
-export const getComments = async (videoId) => {
-  const response = await api.get(`/comments/${videoId}`);
-  return response.data;
-};
-
-export const addComment = async (videoId, text) => {
-  const response = await api.post(`/comments/${videoId}`, { text });
-  return response.data;
-};
-
-export const getUserProfile = async (username) => {
-  const response = await api.get(`/users/${username}`);
-  return response.data;
-};
-
-export const followUser = async (userId) => {
-  const response = await api.put(`/users/follow/${userId}`);
-  return response.data;
-};
-
-export const updateProfile = async (data) => {
-  const response = await api.put('/users/profile', data);
-  return response.data;
-};
+export default api;
