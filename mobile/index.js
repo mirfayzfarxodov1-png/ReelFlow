cf262e9dad0ba16e601a725eff9f416da09bb749
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-06-04 21:05:27 +0500

    Add LogBox warning ignores in index.js
    Ignore specific LogBox warnings in React Native.

--- mobile/index.js
+++ mobile/index.js
@@ -2,4 +2,13 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+// LogBox ignore warnings
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs([
+  'Warning: ...',
+  'ReactNativeFiberHostComponent',
+  'Require cycle:',
+  'VirtualizedLists should never be nested'
+]);
+
 AppRegistry.registerComponent(appName, () => App);
