1c30e6ecbc7ba15578b12b9abff52d1cd7ae130d
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-06-04 21:06:59 +0500

    Create metro.config.js

---
+++ mobile/metro.config.js
@@ -0,0 +1,21 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    minifierConfig: {
+      compress: {
+        drop_console: false,
+      },
+    },
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+  watchFolders: [],
+};
+
+module.exports = mergeConfig(defaultConfig, config);
